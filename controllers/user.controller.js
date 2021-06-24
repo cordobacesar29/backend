cb3e03dd6a53f50a4a4f626aeb34039869c9861e
Emiliano Peralta <emilianop96@hotmail.com>
2021-06-23 22:36:22 -0300

    adding user registration route and controller with input validation

---
+++ controllers/user.controller.js
@@ -0,0 +1,9 @@
+const register = async (req, res) => {
+  try {
+    return;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = { register };
---
+++ utils/validate.js
@@ -0,0 +1,48 @@
+const { check, validationResult } = require('express-validator');
+
+exports.validateInput = [
+  check('firstName')
+    .isString()
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage('first name must not be empty')
+    .bail()
+    .isLength({ min: 3 })
+    .withMessage('minimun 3 characters required')
+    .bail(),
+  check('lastName')
+    .isString()
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage('last name must not be empty')
+    .bail()
+    .isLength({ min: 3 })
+    .withMessage('minimun 3 characters required')
+    .bail(),
+  check('email')
+    .not()
+    .isEmpty()
+    .withMessage('email must not be empty')
+    .bail()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage('must be a valid email')
+    .bail(),
+  check('password')
+    .isLength({ min: 5 })
+    .withMessage('minimun 5 characters required'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorsArray = errors
+        .array()
+        .map(error => ({ field: error.param, message: error.msg }));
+      return res.status(400).json({ errors: errorsArray });
+    }
+    next();
+  },
+];

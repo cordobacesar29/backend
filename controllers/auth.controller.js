d81ab42fda3db428038f75ccc11c0135126ca47a
César Córdoba <cordobacesar29@gmail.com>
2021-06-26 15:26:16 -0300

    auth controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs');
+const models = require('../models');
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const emailExists = await models.User.findOne({ where: { email } });
+    if (emailExists) {
+      const passwordValidate = bcrypt.compareSync(req.body.password, password);
+      if(!passwordValidate) res.status(400).json({ok: false, msj:'User or password incorrect'});
+      res.json(passwordValidate);
+    }else {
+        res.status(400).json({ok: false, msj:'User or password incorrect'});
+    }
+  } catch (error) {
+    return res.status(400).json({ ok: false, error });
+  }
+};
+
+module.exports = { login };

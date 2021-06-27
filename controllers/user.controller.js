5deceb4b831a9e10270ea65a1798561d7cc62340
César Córdoba <cordobacesar29@gmail.com>
2021-06-27 11:53:00 -0300

    add login controller inside user.contrller File

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,4 +17,24 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const emailExists = await models.User.findOne({ where: { email } });
+    if (emailExists) {
+      const passwordValidate =  bcrypt.compareSync(req.body.password, password);
+      if(!passwordValidate) res.status(400).json({ok: false, msj:'User or password incorrect'});
+      const user = await models.User.findOne({ password: passwordValidate });
+      res.json(user);
+    }else {
+        res.status(400).json({ok: false, msj:'User or password incorrect'});
+    }
+  } catch (error) {
+    return res.status(400).json({ ok: false, error });
+  }
+};
+
+module.exports = { 
+  register,
+  login
+ };

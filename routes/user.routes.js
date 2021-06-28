d3a5c394bf0939831bd184c7ed30ffa8c321d3ba
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-28 02:40:12 -0300

    Adding the GET auth/me route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const { validateInput } = require('../utils/validate');
+const { checkToken } = require('../utils/checkToken');
 
 const userController = require('../controllers/user.controller');
 
 const router = express.Router();
 
 router.post('/auth/register', validateInput, userController.register);
+
+router.get('/auth/me', checkToken, userController.userData)
 
 module.exports = router;

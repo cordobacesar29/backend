bf346ea84dea69d7bff9c4e2d77e7f2a6bd91ed2
César Córdoba <cordobacesar29@gmail.com>
2021-06-29 18:30:41 -0300

    use trycatch

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,16 +2,23 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 let checkToken = (req, res, next) =>{
-    let token= req.get('Authorization');
-    jwt.verify(token, config.get('configToken.SEED'),(err, decoded)=>{
-        if(err){
-            return res.status(401).json({
-                err
-            })
-        }
-        res.user = decoded.user;
-        next();
-    })
+    try {
+        let token= req.get('Authorization');
+        jwt.verify(token, config.get('configToken.SEED'),(err, decoded)=>{
+            if(err){
+                return res.status(401).json({
+                    err
+                })
+            }
+            res.user = decoded.user;
+            return next();
+        })
+    } catch (error) {
+        return res.status(401).json({
+            err
+        })
+        
+    }
 };
 
 module.exports = checkToken;

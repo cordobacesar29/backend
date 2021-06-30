77b666d1feaa4d52bdc09f20af3b2f8e54477af2
Emiliano Peralta <emilianop96@hotmail.com>
2021-06-30 20:14:14 -0300

    entry put route added

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -9,4 +9,18 @@ const createEntry = async (req, res) => {
   }
 };
 
-module.exports = { createEntry };
+const updateEntry = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const entryToUpdate = await models.Entry.findOne({ where: { id } });
+    if (!entryToUpdate) {
+      return res.status(404).json({ error: 'entry not found with that ID' });
+    }
+    await entryToUpdate.update(req.body);
+    return res.status(200).json(entryToUpdate);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+};
+
+module.exports = { createEntry, updateEntry };
--- routes/news.routes.js
+++ routes/news.routes.js
@@ -7,5 +7,6 @@ const { validateNewsInput } = require('../utils/validate');
 const router = Router();
 
 router.post('/', validateNewsInput, newsController.createEntry);
+router.put('/:id', newsController.updateEntry);
 
 module.exports = router;

1020d1ae1210eda77d6ce6a1961db27317dbc9fa
Franco Peralta <franco.peralta7@hotmail.com>
2021-07-06 11:07:25 -0300

    Save activity

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/user.routes');
 const sendmailRouter = require('./routes/sendmail.routes');
 const newsRouter = require('./routes/news.routes');
+const activityRouter = require('./routes/activity.routes');
 
 const app = express();
 app.use(cors());
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/news', newsRouter);
 app.use('/api/sendmail', sendmailRouter);
+app.use('/api/activities', activityRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

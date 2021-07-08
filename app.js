7089607e480475277c02a147917b7fdb89fe4e9b
claudiocds1987 <cla8787@gmail.com>
2021-07-08 15:24:01 -0300

    Creacion del endpoint get members

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const usersRouter = require('./routes/user.routes');
 const sendmailRouter = require('./routes/sendmail.routes');
 const newsRouter = require('./routes/news.routes');
 const activityRouter = require('./routes/activity.routes');
+const memberRouter = require('./routes/member.routes');
 
 const app = express();
 app.use(cors());
@@ -30,6 +31,7 @@ app.use('/api/users', usersRouter);
 app.use('/api/news', newsRouter);
 app.use('/api/sendmail', sendmailRouter);
 app.use('/api/activities', activityRouter);
+app.use('/api/members', memberRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

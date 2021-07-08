9655c4f6f7b02b57e638524803ce97641740e8d7
Franco Peralta <franco.peralta7@hotmail.com>
2021-07-08 10:59:24 -0300

    Update activity

--- controllers/activity.controller.js
+++ controllers/activity.controller.js
@@ -21,6 +21,36 @@ const createActivity = async(req = request, res = response) => {
     }
 }
 
+const updateActivity = async(req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+
+    try {
+        const activity = await models.Activities.findByPk(id);
+
+        if(!activity) {
+            return res.status(404).json({
+                ok: false,
+                msg: `No existe actividad con el id ${ id }`
+            });
+        }
+
+        await activity.update(body);
+
+        res.json({
+            ok: true,
+            activity
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hubo un error'
+        });
+    }
+}
+
 module.exports = {
-    createActivity
+    createActivity,
+    updateActivity
 }

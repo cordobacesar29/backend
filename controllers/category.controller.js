5e7693efe7a6f5ee91f6ee96f48f50eea888236e
rahul <dev.rkumaar@gmail.com>
2021-07-12 17:08:57 -0300

    feat: endpoint to list the names of all categories

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -12,3 +12,14 @@ exports.createCategory = async (req, res) => {
 		return res.status(400).json({ error });
 	}
 };
+
+exports.getAllCategories = async (req, res) => {
+	try {
+		const categories = await models.Category.findAll({
+			attributes: ['name'],
+		});
+		return res.status(200).json(categories);
+	} catch (error) {
+		return res.status(400).json({ error });
+	}
+};

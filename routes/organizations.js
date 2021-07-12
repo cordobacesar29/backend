b8e09c715c13805ac411cfd5a5b1a68562388b7a
César Córdoba <cordobacesar29@gmail.com>
2021-07-12 11:24:20 -0300

    add organization controller

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const ORGController = require('../controllers/organization.controller');
 
-router.get('/1/public', (req, res) => {
-    try {
-        return res.status(200).json({
-            name: '', 
-            image: '', 
-            phone: '', 
-            address: '', 
-            welcomeText: '',
-        });
-      } catch (error) {
-        console.log({ error });
-        return res.status(500).json({ mensaje: error.message });
-      }
-});
+
+router.get('/1/public', ORGController.getOrganization);
 
 module.exports = router;

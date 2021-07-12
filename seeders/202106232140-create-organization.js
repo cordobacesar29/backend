782f4efa8d652e7a04cd7ce710a7275e1db1b99e
César Córdoba <cordobacesar29@gmail.com>
2021-07-12 11:25:55 -0300

    fix phone atribute inside organization seeders because this was in nymber type, not in string type

--- seeders/202106232140-create-organization.js
+++ seeders/202106232140-create-organization.js
@@ -6,7 +6,7 @@ module.exports = {
       {
         name: 'organization test',
         image: 'https://images.freeimages.com/images/large-previews/23a/three-gondoliers-and-a-duck-1391336.jpg',
-        phone: 0111552,
+        phone: '0111552',
         address: 'Organization seeders test',
         welcomeText: 'welcome to organization test',
         createdAt: new Date,
@@ -15,7 +15,7 @@ module.exports = {
       {
         name: 'organization test 2',
         image: 'https://images.freeimages.com/images/large-previews/23a/three-gondoliers-and-a-duck-1391336.jpg',
-        phone: 01115554,
+        phone: '01115554',
         address: 'Organization seeders test 2 ',
         welcomeText: 'welcome to organization test 2',
         createdAt: new Date,

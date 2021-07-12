bf17a09f89a8b6addd6197d18c96b55b0b4da208
rahul <dev.rkumaar@gmail.com>
2021-07-12 16:32:08 -0300

    fixed config values and typos for checkToken and isAdmin functions

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,62 +1,60 @@
 const jwt = require('jsonwebtoken');
-const config = require('../config/config');
+const config = require('../config/default.json');
 
-const getTokenDecode = (req)=>{
-    return new Promise((resolve, reject)=>{
-        let token = req.get('Authorization');
-        if(token){
-            if (token.startsWith('Bearer ')) {
-                // Remove Bearer from string
-                token = token.slice(7, token.length);
-            }
-            jwt.verify(token, config.get('configToken.SEED'), (err, decoded)=>{
-                if(err) reject("Without Authorization.")
+const getTokenDecode = (req) => {
+	return new Promise((resolve, reject) => {
+		let token = req.get('Authorization');
+		if (token) {
+			if (token.startsWith('Bearer ')) {
+				// Remove Bearer from string
+				token = token.slice(7, token.length);
+			}
 
-                resolve(decoded)
-            })
-        }
+			jwt.verify(token, config.configToken.SEED, (err, decoded) => {
+				if (err) reject('Without Authorization.');
 
-        reject("Without Authorization.")
-    })
+				resolve(decoded);
+			});
+		}
 
-}
-
-const checkToken = async (req, res, next) =>{
-    try {
-        const decodeJwt =  await getTokenDecode(req)
-
-        res.user = decodeJwt.data;
-
-        return next();
-    } catch (error) {
-        return res.status(401).json({
-            err
-        })
-        
-    }
+		reject('Without Authorization.');
+	});
 };
 
-const isRoleAdmin = (role)=> {
-    const ROLE_ADMIN = 1;
-    
-    return role === ROLE_ADMIN
-}
+const checkToken = async (req, res, next) => {
+	try {
+		const decodeJwt = await getTokenDecode(req);
 
-const isAdmin = async(req, res, next) => {
-    try {
-        const {user} = req;   
-        if(user && isRoleAdmin(user.roleId)){
-            return next();
-        }
-        const decodeJwt =  await getTokenDecode(req)
-        if (isRoleAdmin(decodeJwt.data.role))  return next();
+		res.user = decodeJwt.data;
 
-    } catch (error) {
-       return res.status(400).json({message: error});
-    }
-}
+		return next();
+	} catch (error) {
+		return res.status(401).json({
+			error,
+		});
+	}
+};
+
+const isRoleAdmin = (role) => {
+	const ROLE_ADMIN = 1;
+
+	return role === ROLE_ADMIN;
+};
+
+const isAdmin = async (req, res, next) => {
+	try {
+		const { user } = res;
+		if (user && isRoleAdmin(user.role)) {
+			return next();
+		}
+		const decodeJwt = await getTokenDecode(req);
+		if (isRoleAdmin(decodeJwt.data.role)) return next();
+	} catch (error) {
+		return res.status(400).json({ message: error });
+	}
+};
 
 module.exports = {
-    checkToken,
-    isAdmin
+	checkToken,
+	isAdmin,
 };

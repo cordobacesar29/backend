900ccfeb419aa1c483d230a6b987fcc3e6943d29
César Córdoba <cordobacesar29@gmail.com>
2021-07-14 11:51:15 -0300

    fix validate login input

--- utils/validate.js
+++ utils/validate.js
@@ -171,3 +171,27 @@ exports.validateMemberInput = [
 		next();
 	},
 ];
+
+exports.validateLoginInput = [
+	check('email')
+		.not()
+		.isEmpty()
+		.withMessage('email must not be empty')
+		.bail()
+		.isEmail()
+		.withMessage('must be a valid email')
+		.bail(),
+	check('password')
+		.isLength({ min: 6 })
+		.withMessage('minimun 6 characters required'),
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			const errorsArray = errors
+				.array()
+				.map((error) => ({ field: error.param, message: error.msg }));
+			return res.status(400).json({ errors: errorsArray });
+		}
+		next();
+	},
+];

78abfe4b866f775e97dd17474015d25a8981d65f
Emiliano Peralta <emilianop96@hotmail.com>
2021-07-22 14:09:33 -0300

    send user role at login. apply prettier rules

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,6 @@
 const config = require('config');
 const bcrypt = require('bcryptjs');
-
-const jwt = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const models = require('../models');
 
@@ -9,8 +8,8 @@ const salt = config.get('salt');
 
 const sendMail = require('../utils/sendMail');
 
-const isUser = (user)=> !!user;
-const generatehast = (password) => bcrypt.hashSync(password, salt)
+const isUser = user => !!user;
+const generatehast = password => bcrypt.hashSync(password, salt);
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
@@ -20,33 +19,36 @@ const register = async (req, res, next) => {
       return res.status(400).json({ error: 'email already in use' });
     }
     const hashedPassword = generatehast(password);
-    const user = await models.User.create({ ...req.body, password: hashedPassword });
+    const user = await models.User.create({
+      ...req.body,
+      password: hashedPassword,
+    });
     const msg = {
       to: user.email, // 'to: nombre del email' Email destino
-      subject: "Sent from website contact Form", // 'subject: titulo del texto' Titulo del mensaje
-      text: "Hello", // 'text: contenido del mensaje' Descripción del mensaje
+      subject: 'Sent from website contact Form', // 'subject: titulo del texto' Titulo del mensaje
+      text: 'Hello', // 'text: contenido del mensaje' Descripción del mensaje
       html: `<strong>Gracias por registrarte ${user.firstName} ${user.lastName}!</strong>`,
-    }
+    };
     sendMail(msg);
-    return res.status(201).json({ 
+    return res.status(201).json({
       id: user.id,
       email: user.email,
       firstName: user.firstName,
-      lastName: user.lastName
+      lastName: user.lastName,
     });
   } catch (error) {
     return res.status(400).json({ error });
   }
 };
 
-const getUsers = async( req, res) => {
+const getUsers = async (req, res) => {
   try {
     const data = await models.User.findAll();
     return res.status(200).json(data);
   } catch (error) {
     return res.status(400).json({ error });
   }
-}
+};
 
 const deleteUser = async (req, res) => {
   const { id } = req.params;
@@ -56,8 +58,8 @@ const deleteUser = async (req, res) => {
       return res.status(400).json({ error: 'User to delete does not exist' });
     }
     const deleteUser = await models.User.destroy({
-      where: {id: id}
-    })
+      where: { id: id },
+    });
     return res.status(201).json(deleteUser);
   } catch (error) {
     return res.status(400).json({ error });
@@ -65,68 +67,68 @@ const deleteUser = async (req, res) => {
 };
 
 const userData = async (req, res) => {
-  const userId = req.user.id
-  // Getting user by id
+  const userId = req.user.id;
   try {
     const userData = await models.User.findByPk(userId);
     if (isUser(user)) {
       res.status(404).json({
         ok: false,
-        msg: "User id does not exist",
+        msg: 'User id does not exist',
       });
-    } 
+    }
     res.json(userData);
   } catch (error) {
     return res.status(400).json({ error });
   }
+};
 
-}
-
-const loginAfterRegister = (req, res) =>{
-  const payload = {id: res.locals.id};
+const loginAfterRegister = (req, res) => {
+  const payload = { id: res.locals.id };
   try {
-    const jwToken = jwt.sign(
-      payload,
-      config.get("configToken.SEED"), 
-      { expiresIn: config.get("configToken.expiration")}
-    );
+    const jwToken = jwt.sign(payload, config.get('configToken.SEED'), {
+      expiresIn: config.get('configToken.expiration'),
+    });
     return res.status(201).json({
       ok: true,
-      message: "user registered and authenticated",
-      token: jwToken
+      message: 'user registered and authenticated',
+      token: jwToken,
+      userRole: user.roleId,
     });
   } catch (error) {
     return res.status(401).json({ ok: false, error });
-  }  
-}
+  }
+};
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await models.User.findOne({ where: { email } });
     if (isUser(user)) {
-      const passwordValidate =  bcrypt.compareSync(password , user.password);
-      console.log('passwordvalidate: ', passwordValidate);
-      if(!passwordValidate)   throw new Error('User or password incorrect')
-      
-      const jwToken = jwt.sign({data: {id: user.id, role: user.roleId}},config.get("configToken.SEED"), { expiresIn: config.get("configToken.expiration")});
+      const passwordValidate = bcrypt.compareSync(password, user.password);
+      if (!passwordValidate) throw new Error('User or password incorrect');
+      const jwToken = jwt.sign(
+        { data: { id: user.id, role: user.roleId } },
+        config.get('configToken.SEED'),
+        { expiresIn: config.get('configToken.expiration') }
+      );
       return res.status(200).json({
-        message: "user authenticated",
-        token: jwToken
+        message: 'user authenticated',
+        token: jwToken,
+        userRole: user.roleId,
       });
     }
-    throw new Error('User or password incorrect')
+    throw new Error('User or password incorrect');
   } catch (error) {
     return res.status(400).json({ ok: false, error });
   }
 };
 
-module.exports = { 
+module.exports = {
   register,
   login,
   userData,
   deleteUser,
   loginAfterRegister,
   getUsers,
-  generatehast
- };
+  generatehast,
+};

9becc3c03472795962d2f7adef79594d630387b9
claudiocds1987 <cla8787@gmail.com>
2021-07-26 18:05:58 -0300

    Implement upload middleware

--- controllers/activity.controller.js
+++ controllers/activity.controller.js
@@ -1,67 +1,75 @@
 const models = require('../models');
 
-const getActivityById = async (req, res) => {
-	const { id } = req.params;
-	const activity = await models.Activities.findOne({ where: { id: id } });
 
-	try {
-		if (activity) res.status(200).json(activity);
-		else res.staus(404).json('No activity found with given ID');
-	} catch (error) {
-		return res.status(500).json(error.message);
-	}
+const getActivityById = async (req, res) => {
+  const { id } = req.params;
+  const activity = await models.Activities.findOne({ where: { id: id } });
+
+  try {
+    if (activity) res.status(200).json(activity);
+    else res.staus(404).json('No activity found with given ID');
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 };
 
 const createActivity = async (req, res) => {
-	const { body } = req;
+  const { filename } = req.file;
+  const { name, content } = req.body;
 
-	try {
-		const activitySave = await models.Activities.create(body);
+  const newActivity = {
+    image: filename,
+    name,
+    content,
+  };
 
-		res.json({
-			ok: true,
-			activity: activitySave,
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({
-			ok: false,
-			msg: 'Hubo un error',
-		});
-	}
+  try {
+    const activitySave = await models.Activities.create(newActivity);
+
+    res.json({
+      ok: true,
+      activity: activitySave,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hubo un error',
+    });
+  }
 };
 
 const updateActivity = async (req, res) => {
-	const { id } = req.params;
-	const { body } = req;
+  const { id } = req.params;
+  const { body } = req;
 
-	try {
-		const activity = await models.Activities.findByPk(id);
+  try {
+    const activity = await models.Activities.findByPk(id);
 
-		if (!activity) {
-			return res.status(404).json({
-				ok: false,
-				msg: `No existe actividad con el id ${id}`,
-			});
-		}
+    if (!activity) {
+      return res.status(404).json({
+        ok: false,
+        msg: `No existe actividad con el id ${id}`,
+      });
+    }
 
-		await activity.update(body);
+    await activity.update(body);
 
-		res.json({
-			ok: true,
-			activity,
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({
-			ok: false,
-			msg: 'Hubo un error',
-		});
-	}
+    res.json({
+      ok: true,
+      activity,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hubo un error',
+    });
+  }
 };
 
 module.exports = {
-	createActivity,
-	updateActivity,
-	getActivityById,
+  createActivity,
+  updateActivity,
+  getActivityById,
 };
--- routes/activity.routes.js
+++ routes/activity.routes.js
@@ -2,34 +2,38 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const {
-	createActivity,
-	updateActivity,
-	getActivityById,
+  createActivity,
+  updateActivity,
+  getActivityById,
 } = require('../controllers/activity.controller');
 const { validateFields } = require('../middlewares/validateFields');
+
+const upload = require('../middlewares/upload');
 
 const router = express.Router();
 
 router.get('/:id', getActivityById);
 
 router.post(
-	'/',
-	[
-		check('name', 'El nombre es obligatorio').not().isEmpty(),
-		check('content', 'El contenido es obligatorio').not().isEmpty(),
-		validateFields,
-	],
-	createActivity
+  '/',
+  upload,
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('content', 'El contenido es obligatorio').not().isEmpty(),
+    validateFields,
+  ],
+  createActivity
 );
 
 router.put(
-	'/:id',
-	[
-		check('name', 'El nombre es obligatorio').not().isEmpty(),
-		check('content', 'El contenido es obligatorio').not().isEmpty(),
-		validateFields,
-	],
-	updateActivity
+  '/:id',
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('content', 'El contenido es obligatorio').not().isEmpty(),
+    validateFields,
+  ],
+  updateActivity
 );
 
 module.exports = router;
+

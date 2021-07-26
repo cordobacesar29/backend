cb9a11ee17fe3c508e849988ce22b7e0f1f41bcc
claudiocds1987 <cla8787@gmail.com>
2021-07-26 18:07:49 -0300

    Formated

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const {checkToken, isAdmin} = require('../middlewares/auth');
+const { checkToken, isAdmin } = require('../middlewares/auth');
 
 const { validateRegisterInput } = require('../utils/validate');
 const { validateLoginInput } = require('../utils/validate');
 
 const userController = require('../controllers/user.controller');
 
-
 const router = express.Router();
 
-router.post('/auth/register', validateRegisterInput, userController.register);
+router.post(
+  '/auth/register',
+  validateRegisterInput,
+  userController.register,
+  userController.loginAfterRegister
+);
 router.post('/auth/login', validateLoginInput, userController.login);
-router.get('/auth/me', checkToken, userController.userData)
+router.get('/auth/me', checkToken, userController.userData);
 
 router.get('/', [checkToken, isAdmin], userController.getUsers);
-router.delete('/users/:id', isAdmin, userController.deleteUser );
+router.delete('/users/:id', isAdmin, userController.deleteUser);
 
 module.exports = router;

34283730d2d722dd114fa00a86d01ca65e133a3b
Emiliano Peralta <emilianop96@hotmail.com>
2021-07-26 14:49:45 -0300

    increase the mamixum number of characters allowed

--- models/organization.js
+++ models/organization.js
@@ -2,37 +2,37 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-	class Organization extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		static associate(models) {
-			Organization.hasMany(models.User, {
-				foreignKey: 'organizationId',
-				as: 'user',
-			});
-			Organization.hasOne(models.SocialMedia, {
-				foreignKey: 'organizationId',
-				as: 'socialMedia',
-			});
-		}
-	}
-	Organization.init(
-		{
-			name: DataTypes.STRING,
-			image: DataTypes.STRING,
-			phone: DataTypes.INTEGER,
-			address: DataTypes.STRING,
-			welcomeText: DataTypes.STRING,
-			deletedAt: DataTypes.DATE,
-		},
-		{
-			sequelize,
-			modelName: 'Organization',
-			paranoid: true,
-		}
-	);
-	return Organization;
+  class Organization extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Organization.hasMany(models.User, {
+        foreignKey: 'organizationId',
+        as: 'user',
+      });
+      Organization.hasOne(models.SocialMedia, {
+        foreignKey: 'organizationId',
+        as: 'socialMedia',
+      });
+    }
+  }
+  Organization.init(
+    {
+      name: DataTypes.STRING,
+      image: DataTypes.STRING,
+      phone: DataTypes.INTEGER,
+      address: DataTypes.STRING,
+      welcomeText: DataTypes.STRING(1000),
+      deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: 'Organization',
+      paranoid: true,
+    }
+  );
+  return Organization;
 };

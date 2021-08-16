169dc3a5bb6fdc8afc8ec83622342e8232b74651
César Córdoba <cordobacesar29@gmail.com>
2021-08-16 13:36:24 -0300

    delete routes and add proyectRouter

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,11 @@
 const usersRouter = require('./user.routes');
-const sendmailRouter = require('./sendmail.routes');
-const newsRouter = require('./news.routes');
-const testimonyRouter = require('./testimony.routes');
-const activityRouter = require('./activity.routes');
 const organizationsRouter = require('./organization.routes');
-const memberRouter = require('./member.routes');
-const contactRouter = require('./contact.routes');
-const categoryRouter = require('./category.routes');
-const sliderRouter = require('./slider.routes');
+const proyectRouter = require('./proyect.routes');
 
 const createRoutes = app => {
   app.use('/api/users', usersRouter);
-  app.use('/api/news', newsRouter);
-  app.use('/api/sendmail', sendmailRouter);
-  app.use('/api/testimonials', testimonyRouter);
-  app.use('/api/activities', activityRouter);
   app.use('/api/organizations', organizationsRouter);
-  app.use('/api/members', memberRouter);
-  app.use('/api/contact', contactRouter);
-  app.use('/api/categories', categoryRouter);
-  app.use('/api/sliders', sliderRouter);
+  app.use('/api/proyects/', proyectRouter);
 };
 
 module.exports = createRoutes;

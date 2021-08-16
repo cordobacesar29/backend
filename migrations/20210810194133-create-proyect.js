54e2725ec534a70cae7d5a9e0de33c7269243d46
César Córdoba <cordobacesar29@gmail.com>
2021-08-16 13:37:12 -0300

    create proyect migration

---
+++ migrations/20210810194133-create-proyect.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('Proyects', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				type: Sequelize.STRING,
+			},
+			description: {
+				type: Sequelize.STRING,
+			},
+			repositore: {
+				type: Sequelize.STRING,
+			},
+			link: {
+				type: Sequelize.STRING,
+			},
+			image: {
+				type: Sequelize.STRING,
+			},
+			deletedAt: {
+				type: Sequelize.DATE,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('Proyects');
+	},
+};
